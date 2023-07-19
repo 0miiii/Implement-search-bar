d5aebeabb940a39106459c3046439ce6924d8150
mini <0miiii@naver.com>
2023-07-19 18:16:58 +0900

    feat #1 API 호출별로 로컬 캐싱 구현

--- src/services/search.ts
+++ src/services/search.ts
@@ -1,4 +1,4 @@
-import { createHttpClient } from "../utils";
+import { createHttpClient, createCache } from "../utils";
 import { sick } from "../types";
 
 interface SearchService {
@@ -6,11 +6,23 @@ interface SearchService {
 }
 
 const baseURL = "http://localhost:4000";
+const searchCache = createCache<sick[]>();
 const searchClient = createHttpClient(baseURL);
 
 export const searchService: SearchService = async (query) => {
+  if (!query) {
+    return [];
+  }
+
+  const cachedData = searchCache.get(query);
+
+  if (cachedData) {
+    return cachedData;
+  }
+
   try {
     const { data } = await searchClient.get(`/sick?q=${query}`);
+    searchCache.set(query, data);
     return data;
   } catch (error) {
     console.error("데이터 요청 실패:", error);
