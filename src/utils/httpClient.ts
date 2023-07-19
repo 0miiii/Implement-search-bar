ea0efe84466c62ed3821b722c315fa09c3380827
mini <0miiii@naver.com>
2023-07-19 13:41:08 +0900

    feat: httpClient 생성
    instance는 baseURL을 인자로 받으며 에러 처리를 합니다

---
+++ src/utils/httpClient.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance, AxiosError } from "axios";
+
+interface HttpClient {
+  (baseURL: string): AxiosInstance;
+}
+
+export const createHttpClient: HttpClient = (baseURL) => {
+  const httpClient = axios.create({
+    baseURL: baseURL,
+    timeout: 5000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  httpClient.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response) {
+        console.error("API 오류 응답:", error.response.data);
+        console.error("응답 상태 코드:", error.response.status);
+      } else if (error.request) {
+        console.error("API 응답 없음:", error.request);
+      } else {
+        console.error("API 요청 오류:", error.message);
+      }
+      return Promise.reject(error);
+    },
+  );
+
+  return httpClient;
+};
