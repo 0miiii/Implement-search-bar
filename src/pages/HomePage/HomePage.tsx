12aa4904cfe165b57563ef5ceccbc1e3198c465a
mini <0miiii@naver.com>
2023-07-19 14:49:10 +0900

    feat: #1 검색할 때 데이터 페칭 및 추천 검색어 렌더링
    데이터를 페치할 때 console에 calling api가 출력됩니다

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,32 @@
+import { useState } from "react";
+import { searchService } from "../../services";
+import { Input, RecommendedSearch } from "../../components";
+import { sick } from "../../types";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const [searchResult, setSearchResult] = useState<sick[]>([]);
+
+  const fetchData = async (query: string) => {
+    console.info("calling api");
+    try {
+      const response = await searchService(query);
+      setSearchResult(response);
+    } catch (error) {
+      console.error("데이터 요청 실패:", error);
+    }
+  };
+
+  const fetchDataByInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    fetchData(event.target.value);
+  };
+
+  return (
+    <div>
+      HomePage
+      <Input onChange={fetchDataByInputChange} />
+      <RecommendedSearch sickInfo={searchResult} />
+    </div>
+  );
 };
